607e5cb7e0fd33acec3927ca653ec39ca42e604f
Eishal <eishal.cecos@gmail.com>
2025-04-15 11:04:24 +0500

    d
    d

---
+++ 3.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+Voidm main() {
+  int marks;
+  stdout.write("Enter Marks to check grade:");
+
+  if (marks == 20 && marks == 40) {
+    print("Grade : Fail");
+  } else if (marks == 41 && marks == 61) {
+    print("Grade: C");
+  } else if (marks == 62 && marks == 80) {
+    print("Grade : B");
+  } 
+  else if(marks == 81 && marks == 100){
+    print("Grade : A");
+  }
+  else {
+    print("wrong");
+  }
+
+}
