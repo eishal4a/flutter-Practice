bdab39631f155d31cf9e5b202155944bc1930d2d
Eishal <eishal.cecos@gmail.com>
2025-04-20 16:41:03 +0500

    p
    p

--- 6.dart
+++ 6.dart
@@ -1,6 +1,18 @@
 import 'dart:io';
 
-void main(){
+void main() {
+  int x;
 
-  
+  stdout.write("Enter a value:");
+  x = int.parse(stdin.readLineSync()!);
+
+  if (x> 0) {
+    print('This is positive number');
+  }
+  else if (x<0) {
+    print('This is negative number');
+  }
+  if (x == 0) {
+    print('This is zero');
+  }
 }
