0ffb799f08d8cabb121697036d45d5190fa1dcbf
Eishal <eishal.cecos@gmail.com>
2025-04-20 16:25:18 +0500

    p
    p

--- 1.dart
+++ 1.dart
@@ -1,21 +1,17 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Enter your name:');
+  stdout.write('Enter your name: ');
   String? name = stdin.readLineSync();
 
-  stdout.write('Enter your age:');
+  stdout.write('Enter your age: ');
   int age = int.parse(stdin.readLineSync()!);
 
-  stdout.write('Enter your height in feet:');
-  double height = double.parse(stdin.readLineSync()!);
-
-  stdout.write('Enter your weight(in kg):');
-  double weight = double.parse(stdin.readLineSync()!);
+  stdout.write('Enter your CGPA: ');
+  double cgpa = double.parse(stdin.readLineSync()!);
 
   print('\n--- User Information ---');
   print('Name: $name');
-  print('Name: $age');
-  print('Name: $height feet');
-  print('Name: $weight kg');
+  print('Age: $age');
+  print('Height: $cgpa');
 }
